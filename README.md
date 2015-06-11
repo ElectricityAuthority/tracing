4a7c2d4ee029b83fbcba01ae805422a11f0cf858
David Hume <djhume@gmail.com>
2015-06-11 17:54:59 +1200

    Initial setup of public trace repo

---
+++ README.md
@@ -0,0 +1,104 @@
+# Flow tracing of electricity in New Zealand
+
+This repository contains code to run the power flow tracing routine that is used as part of the Electricity Authority's proposed Transmission Pricing Methodology (TPM).
+
+Transmission asset usage can be used in the Market Design of a Transmission Pricing Methodology.  In this case, it is being used in determining those assets that could be considered "deep" connection assets.  I.e., those assets that are shared among only a few participants.
+
+## Inputs:
+
+The only required inputs for this calculation are the MW power flows and directions on each transmission element, the load at load buses and the generation at generation buses. Each transmission element must have a unique identifier (i.e., a TO and FROM bus) so the tracing methodology is aware of the network topology.
+ 
+These inputs are the outputs of a solved SPD or vSPD market model.  So the tracing methodology follows the market solve and is applied at trading period level.
+
+In this case the input data is from a Concept Consulting vSPD run and in a slightly altered format to the output data found from a generic vSPD run.  
+
+The input data for 3 years of data runs to many GB in size.  It has been split into manageable monthly files which can be downloaded from the EA EMI website.
+
+A sample of the input data can be found within this repository at /data/input along with a fuller description of the input data which also includes a few mapping files required for the tracing routine. 
+
+
+## Outputs
+
+Through the use of what Bialek described as the proportional sharing principle, Flow Tracing is able to calculate the share of usage of transmission assets.  It is able to allocate usage in two directions: 
+
+  - an "upstream" trace; where transmission asset usage is allocated to grid connected generators, and,
+  - a "downstream" trace which is the exact opposite.  I.e., where transmission asset usage is allocated downstream to demand/load. 
+  
+As discussed above, this is achieved at both trading period level and at GXP/GIP level.  It is a simple matter to then group and sum this GXP/GIP level data to a participant level.  In this instance, this code does this for the downstream trace to local lines company level.
+
+Once this program is run, the output files should appear in the data/output directory.  Although the trace is applied at a Trading Period level, the output files are saved as the mean all trading periods during each day.  Each day four output files are saved of the following form:
+
+  - td_YYYYMMDD.csv   "downstream" trace results that allocate transmission circuits and transformers to off-take/demand GXPs.
+  - tu_YYYYMMDD.csv   "upstream" trace that allocate transmission circuits and transformers to generation GIPs.. 
+  - sd_YYYYMMDD.csv   "downstream" trace which allocates substations to off-take/demand GXPs. 
+  - su_YYYYMMDD.csv   "upstream" trace which allocates substations to generation GIPs. 
+
+
+## Installation instructions
+
+As of 9/6/2015, this code has only been tested on a Linux machine running Ubuntu 15.04.  We may test on a windows machine in the future.
+To run this code requires an installation of python 2.7 with the python modules as indicated in the requirements.txt file. 
+
+The best way to run this code is from within a virtual environment using the virtualenv python package.  This allows the required dependencies in the requirements.txt file to be installed independently from any other python installation on the host machine.  
+
+You can install virtualenv via pip:
+````
+$ pip install virtualenv
+```
+To clone this repository, open a terminal window and type/paste the following commands;
+``` 
+$git clone https://electricityauthority/tpm
+$cd tpm
+```
+Once the repository has downloaded, we can set up a virtual environment in the local directory called env by typing: 
+```
+$virtualenv env
+```
+We activate the virtual environment, by typing;
+```
+$source env/bin/activate
+```
+Running the trace now would cause errors as the trace.py is dependent on a number of other python modules.  
+To solve this we install the following packages into the virtual environment by typing:
+```
+$pip install numpy
+$pip install scipy
+$pip install pandas
+$pip install psutil 
+
+```
+We should now be able to run the trace over the sampled data supplied.
+
+To run the trace, issue the following command:
+```
+./trace.py
+```
+All going well, the trace should start and you should see confirmation in the terminal. 
+Daily output files will be generated for one month. 
+
+Adding Input files from the EMI website into the /data/input/vSPDout directory and editing trace.py (test_limit_min and test_limit_max) should allow the program to run over other time periods. 
+
+## using on vSPD daily output files
+
+A slightly altered version of this code has been written to take advantage of the daily vSPD output files and this can be used for tracing historic output.  A slight modification to the output is required and for this reason a small gams script is also required to output additional bus/node mapping data.  The EA may also publish this code in the future.
+
+
+## Additional background information
+
+Electricity tracing was formally described by Janusz W. Bialek in his classic 1996 paper titled Tracing the flow of electricity.
+
+The method has since been applied in NZ by the Electricity Commission in 2010 as part of the on-going Transmission Pricing Methodology.  To aid in understanding the methodology, a paper was written at the time and is still available via the EA website at:
+
+https://www.ea.govt.nz/dmsdocument/7123
+
+More recently this methodology has now been updated, using open source software, as part of the more recent Transmission Pricing options work currently being conducted by the Market Design team within the Electricity Authority.
+
+For further information on technical aspects of this code, contact:
+
+info@ea.govt.nz
+
+
+
+
+
+
---
+++ trace.py
@@ -0,0 +1,397 @@
+#!/usr/bin/python
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from scipy.linalg import inv
+import logging as l
+from psutil import phymem_usage
+import os
+import traceback
+
+
+# Setup some console logging
+formatter = l.Formatter('|%(asctime)-6s|%(message)s|', '%Y-%m-%d %H:%M:%S')
+conlog = l.StreamHandler()
+conlog.setLevel(l.INFO)
+conlog.setFormatter(formatter)
+l.getLogger('').addHandler(conlog)
+logger = l.getLogger('TRACE')
+logger.setLevel(l.INFO)
+
+
+def load_vSPD_data(vSPD_b, vSPD_n, mappings=True):
+    """Function that loads vSPD data"""
+    def bmmap(x):
+        """Extra nodes inserted by Concept"""
+        bm = {'BEN0162': 6000,
+              'BEN0163': 6001,
+              'TKB2202': 6002,
+              'GOR2201': 6003,
+              'TKA0112': 6004,
+              'WRU2201': 6005,
+              'HOB2201': 6006}
+        if x in bm.keys():
+            return bm[x]
+        else:
+            return int(x)
+
+    # Load annual Node/Bus data
+    n = pd.read_csv(vSPD_n, header=None, index_col=0, parse_dates=True,
+                    names=['tp', 'node', 'bus', 'allofact', 'GENERATION',
+                           'LOAD', 'bidMW'])
+    n.bus = n.bus.map(lambda x: bmmap(x))
+    # Load branch flow data
+    b = pd.read_csv(vSPD_b, header=None, index_col=0,
+                    names=['tp', 'branch', 'FROM_MW', 'DynamicLoss (MW)',
+                           'FixedLoss (MW)', 'FROM_ID_BUS', 'TO_ID_BUS'],
+                    parse_dates=True)
+    b.FROM_ID_BUS = b.FROM_ID_BUS.map(lambda x: bmmap(x))
+    b.TO_ID_BUS = b.TO_ID_BUS.map(lambda x: bmmap(x))
+    if mappings:
+        # Get some mappings
+        nmap = n.ix[:, ['node', 'bus']].reset_index(drop=True)\
+                .drop_duplicates().set_index(['bus']).sort_index().node
+        nmap2 = nmap.groupby(level=0).first()
+        brmap = b.ix[:, ['branch', 'FROM_ID_BUS', 'TO_ID_BUS']]\
+                .reset_index(drop=True).drop_duplicates()\
+                .set_index(['FROM_ID_BUS', 'TO_ID_BUS'])['branch']
+    # Further munging
+    n = n.set_index(['tp', 'node', 'bus'], append=True)
+    n['LOAD'] = n.allofact * (n.LOAD + n.bidMW)
+    n['GENERATION'] = n.allofact * n.GENERATION
+    n = n.drop(['allofact', 'bidMW'], axis=1)
+    b['TO_MW'] = b['FROM_MW'] - b['DynamicLoss (MW)']
+    b = b.set_index(['tp', 'branch', 'FROM_ID_BUS', 'TO_ID_BUS'], append=True)
+    b = b.ix[:, ['FROM_MW', 'TO_MW']]
+
+    if mappings:
+        return n, b, nmap, nmap2, brmap
+    else:
+        return n, b
+
+
+def A(b, n, downstream=True):
+    """Given branch flows and load/generation build the A matrix
+        Note: similar to Amatrix.m """
+    b = b.ix[:, ['FROM_MW', 'TO_MW']]  # grab the columns of interest
+    allbus = list(set(b.index.levels[0]) | set(b.index.levels[1]))
+    totbus = len(allbus)
+    pg = n.GENERATION.ix[allbus].fillna(0.0).groupby(level=0).sum()
+    pl = n.LOAD.ix[allbus].fillna(0.0).groupby(level=0).sum()
+    A = pd.DataFrame(np.identity(totbus), index=allbus,
+                     columns=allbus).fillna(0.0)
+    if downstream:
+        """Build Nett downstream Ad with inflows"""
+        b1 = b.ix[b.FROM_MW < 0].FROM_MW  # FROM_MW neg, into bus
+        b2 = b.swaplevel(0, 1).sort()  # swap levels and sort
+        b2 = -b2.ix[b2.TO_MW > 0].TO_MW   # TO_MW pos, into bus
+    else:
+        """Build Gross upstream Au with outflows"""
+        b1 = b.ix[b.TO_MW < 0].TO_MW
+        b2 = b.swaplevel(0, 1).sort()
+        b2 = -b2.ix[b2.FROM_MW > 0].FROM_MW
+
+    b2.index.names = ['FROM_ID_BUS', 'TO_ID_BUS']  # rename to match flow
+    cji = b1.append(b2).sortlevel()  # append and sort
+    cji = cji.groupby(level=[0, 1]).sum()  # sum parallel branches
+
+    # calculate Nodal through-flows using inflows
+    inflow1 = -b.ix[b.FROM_MW < 0].groupby(level=0).sum().FROM_MW  # out
+    inflow2 = b.ix[b.TO_MW > 0].groupby(level=1).sum().TO_MW
+    inflow = pd.DataFrame({'inflow1': inflow1, 'inflow2': inflow2})\
+               .fillna(0.0).sum(axis=1)
+    # calculate Nodal through-flows using outflows
+    outflow1 = b.ix[b.FROM_MW > 0].groupby(level=0).sum().FROM_MW
+    outflow2 = -b.ix[b.TO_MW < 0].groupby(level=1).sum().TO_MW
+    outflow = pd.DataFrame({'outflow1': outflow1,
+                            'outflow2': outflow2}).fillna(0.0).sum(axis=1)
+    Pi = pd.DataFrame({'inflow': inflow, 'outflow': outflow}).fillna(0.0)
+    PI = pd.DataFrame({'Pi+pg': Pi.inflow.add(pg,
+                                              fill_value=0).fillna(0.0),
+                       'Pi+pl': Pi.outflow.add(pl,
+                                               fill_value=0).fillna(0.0)})
+    if downstream:
+        """Build Nett downstream Ad with inflows"""
+        for i, r in pd.DataFrame({'cji': cji}).iterrows():
+            v = r.values/(PI['Pi+pl'].ix[i[0]])
+            A.ix[i[1], i[0]] = v
+    else:
+        """Build Gross upstream Au with outflows"""
+        for i, r in pd.DataFrame({'cji': cji}).iterrows():
+            A.ix[i[0], i[1]] = r.values/(PI['Pi+pg'].ix[i[1]])
+
+    A = A.replace([np.inf, -np.inf], np.nan).fillna(0.0)
+    iA = inv(A)
+    Pi = PI['Pi+pg'].ix[allbus]
+    if downstream:  # calculate the nett generation
+        pgn = iA.dot(np.array(pl.values))*np.array(pg.values) \
+            * np.array(((1 / Pi).fillna(0.0)).values)
+        pg = pd.Series(pgn, index=allbus).fillna(0.0)
+    else:  # calculate the gross demand
+        plg = iA.dot(np.array(pg.values)) * np.array(pl.values) \
+            * np.array(((1 / Pi).fillna(0.0)).values)
+        pl = pd.Series(plg, index=allbus).fillna(0.0)
+    return A, iA, pg, pl, b2, cji, Pi
+
+
+def topo(iA, pi, bd, plg, downstream=True):
+    """Calculate the topological distribution matrices"""
+    # common to both gross-up and nett-down
+    allbus = list(set(bd.index.levels[0]) | set(bd.index.levels[1]))
+    totbus = len(allbus)
+    bdd = bd.groupby(level=[0, 1]).sum().dropna()
+    bpos = bdd.TO_MW >= 0  # Masks that depend on flow direction
+    bneg = bdd.TO_MW < 0
+    bposx = np.array([bpos.values, ] * totbus).transpose()
+    bnegx = np.array([bneg.values, ] * totbus).transpose()
+    ibus = bdd.reset_index().TO_ID_BUS.values
+    jbus = bdd.reset_index().FROM_ID_BUS.values
+    pii = np.array([pi.ix[ibus].values, ] * totbus).transpose()
+    pij = np.array([pi.ix[jbus].values, ] * totbus).transpose()
+    b_in = bdd.TO_MW.values
+    b_out = bdd.FROM_MW.values
+    b_inx = np.array([b_in, ]*totbus).transpose()
+    b_outx = np.array([b_out, ]*totbus).transpose()
+    if downstream:  # Calculate nett branch flows for downstream to demand
+        iAd_df = pd.DataFrame(iA, index=allbus, columns=allbus)
+        i_Ad_ibus = iAd_df.ix[ibus, :]
+        i_Ad_jbus = iAd_df.ix[jbus, :]
+        Pdd = [plg.values, ]*len(b_in)
+        DDilk1 = np.abs(b_inx) * i_Ad_jbus.values * Pdd * (1 / pij)
+        DDilk2 = np.abs(b_outx) * i_Ad_ibus.values * Pdd * (1 / pii)
+        dfd = DDilk1*bnegx + DDilk2*bposx
+        dfd = pd.DataFrame(dfd, index=bdd.index, columns=allbus).fillna(0.0)
+        idx = list(set(dfd.columns) & set(nmap.index))  # filter columns
+        df = dfd[idx]
+    else:  # Calculate gross branch flows for upstream to generators
+        iAu_df = pd.DataFrame(iA, index=allbus, columns=allbus)
+        i_Au_ibus = iAu_df.ix[ibus, :]
+        i_Au_jbus = iAu_df.ix[jbus, :]
+        Pgd = [plg.values, ]*len(b_in)
+        DGilk1 = np.abs(b_inx) * i_Au_ibus.values * Pgd * (1 / pii)
+        DGilk2 = np.abs(b_outx) * i_Au_jbus.values * Pgd * (1 / pij)
+        dfg = DGilk1*bposx + DGilk2*bnegx
+        dfg = pd.DataFrame(dfg, index=bdd.index, columns=allbus).fillna(0.0)
+        idx = list(set(dfg.columns) & set(nmap.index))  # filter columns
+        df = dfg[idx]
+    # Integerize to save memory requirements
+    # df = df.applymap(lambda x: int(100*x))
+    return df
+
+
+def bustocomp(df, nmap, brmap, NPmap, node=False):
+    """Given bus level data, group up to either ELB level,
+        when node=False, or node level when node=True and map to branch
+        names"""
+    # Reindex no node labels
+    df.index = df.index.map(lambda x: brmap[x])
+    df.columns = df.columns.map(lambda x: nmap[x])
+    # select rows and columns that sum to greater than 0
+    df = df.ix[df.sum(axis=1) > 0, df.sum() > 0]
+    # Sum columns to the node label level
+    df = df.groupby(level=0, axis=1, sort=False).sum()
+
+    def seriestolist(x):
+        """We get pd.Series objects when mapping to branch name level,
+            objects from series to array types"""
+        if isinstance(x, type(pd.Series())):
+            idx = x.values
+            return ', '.join(str(e) for e in idx)
+        else:
+            return x
+    df.index = df.index.map(lambda x: seriestolist(x))
+    if node:
+        return df
+    else:  # Sum by ELB
+        df.columns = df.columns.map(lambda x: NPmap[x[0:7]])
+        df = df.groupby(level=0, axis=1, sort=False).sum()
+        return df
+
+
+def trans_use(b, n, nmap, brmap, NPmap, downstream=True):
+    """Subroutine to calculate transmission usage matrix
+       Note: needs re factoring for better readability/McCabe complexity..."""
+
+    if downstream:  # Net downstream pg is nett of losses, pl is actual
+        Ad, iAd, pg, pl, bdd, cjid, Pi = A(b, n, downstream=downstream)
+        df = topo(iAd, Pi, b, pl, downstream=True)
+        df1 = bustocomp(df.copy(), nmap, brmap, NPmap, node=True)  # Node level,
+        df2 = bustocomp(df.copy(), nmap, brmap, NPmap)  # ELB level
+    else:  # Gross upstream pg is actual, pl grossed with losses
+        Au, iAu, pg, pl, bdu, cjiu, Pi = A(b, n, downstream=downstream)
+        df = topo(iAu, Pi, b, pg, downstream=False)
+        df1 = bustocomp(df.copy(), nmap, brmap, NPmap, node=True)  # Node level,
+        df2 = bustocomp(df.copy(), nmap, brmap, NPmap)  # ELB level
+
+    return df, df1, df2, pl, pg
+
+
+def sub_usage(df, pl, pg, nmap, NPmap):
+    """Subroutine to calculate substation usage matrix
+       As trace is applied at bus level, this groups first to node level, then
+       to substation level
+       Note: MW values are summed at substation level so MW numbers represent
+       the total through flow through all buses that comprise a substation
+       - there can be multiple buses/nodes that make a substation to these flows
+       are not really representative or useful for anything other than
+       calculating usage.  This can be improved."""
+
+    def submapping(NPmap):
+        """Determine the ELB/substation mapping given node/ELB mapping """
+        submap = pd.DataFrame({'comp': NPmap})
+        submap.index = submap.index.map(lambda x: x[0:3])
+        submap = submap.reset_index().drop_duplicates()\
+                       .set_index('index').comp.to_dict()
+        submapex = {'RPO': 'GENE'}
+        submap = dict(submap.items() + submapex.items())
+        return submap
+
+    def bus_trace_usage(df, pl, pg, nmap):
+        """Determine bus usage using traced MW from branch dfd"""
+        bus0 = df.groupby(level=0).sum()/2.0
+        bus1 = df.groupby(level=1).sum()/2.0
+        bus = bus0.append(bus1).groupby(level=0).sum()
+        # Need to add half the generation and load to their buses...
+        idx = np.array(list(set(bus.columns) | set(bus.index)))
+        Lx = pd.DataFrame(np.identity(len(idx)), index=idx, columns=idx)
+        pl = pl.ix[idx]
+        pg = pg.ix[idx]
+        Lxx = Lx * pl/2.0 + Lx * pg/2.0
+        bus_usage = (bus.ix[nmap.index, nmap.index] +
+                     Lxx.ix[nmap.index, nmap.index]).fillna(0.0)
+        return bus_usage
+
+    def bus2node(df, nmap, NPmap):
+        """Given bus level data, group up to node level"""
+        # Reindex no node labels
+        df.index = df.index.map(lambda x: nmap[x])
+        df.columns = df.columns.map(lambda x: nmap[x])
+        return df
+
+    def node2sub(df, NPmap, submap):
+        """Given node level data, group up to substation level"""
+        df = df.ix[df.sum(axis=1) > 0, df.sum() > 0]
+        df = df.groupby(df.columns.map(lambda x: NPmap[x[0:7]]),
+                        axis=1, sort=False).sum()
+        df = df.groupby(df.index.map(lambda x: x[0:3]), sort=False).sum()
+        return df
+
+    b_usage = bus_trace_usage(df, pl, pg, nmap)
+    n_usage = bus2node(b_usage, nmap, NPmap)
+    s_usage = node2sub(n_usage, NPmap, submapping(NPmap))
+
+    return n_usage, s_usage
+
+
+###############################################################################
+# Start TRACE
+#
+# Loop through monthly branch and node files, we have to do this because we can
+# not fit all data (3 years worth) into memory all at once.  So we loop over
+# months.  Note: used a cool gawk command line script to split to monthlies...
+#
+# For each monthly data set, we loop over trading periods.  Note: it would be
+# much better suited (cleaner and faster) to use a DataFrame .groupby and .apply
+# methods for this... future todo... for now we loop...
+#
+# For each TP;
+#   - perform UP stream trace for generation usage of transmission assets
+#   - perform DOWN stream trace for load/demand usage of transmission assets.
+#
+# To do this we, initiate collection dictionaries then loop over the input data.
+#
+# D J Hume, Dec 2014.
+#
+###############################################################################
+
+# Set input and output paths
+path = os.getcwd()
+inpath = path + '/data/input/vSPDout/'
+mappath = path + '/data/input/maps/'
+outpath = path + 'data/output/'
+
+# Load data mappings
+
+NPmap = pd.read_csv(mappath + '/elb2gxp.csv', index_col=0,
+                    header=None)[1].to_dict()
+brmap = pd.read_csv(mappath + '/brmap.csv', index_col=[0, 1], header=None)[2]
+nmap = pd.read_csv(mappath + '/busnode.csv', index_col=0, header=None)[1]
+nmap2 = pd.read_csv(mappath + '/busnode2.csv', index_col=0, header=None)[1]
+
+
+logger.info(20*'*')
+logger.info("Start tracing routine")
+logger.info(20*'*')
+fc = {}  # failed counter
+test_limit_min = datetime(2011, 1, 1)
+test_limit_max = datetime(2011, 1, 31)
+for y in [2011, 2012, 2013]:
+    for m in range(1, 13):  # load monthly data
+        if (m <= test_limit_max.month) & (m >= test_limit_min.month):
+            ym = str(y) + str(m).zfill(2) + '.csv'
+            vSPD_b = inpath + "b_" + ym
+            vSPD_n = inpath + "n_" + ym
+            info_text = 'INPUT: b_' + ym + ', n_' + ym
+            logger.info(info_text)
+            n, b = load_vSPD_data(vSPD_b, vSPD_n, mappings=False)
+            for day in n.index.levels[0]:
+                """Possible update. Use groupby/apply on pd.Dataframe."""
+                td = {}  # downstream transmission usage
+                sd = {}  # downstream substation usage
+                tu = {}  # upstream transmission usage
+                su = {}  # upstream substation usage
+                ymd = str(y) + str(m).zfill(2) + str(day.day).zfill(2) + '.csv'
+                if (day <= test_limit_max) & (day >= test_limit_min):
+                    for tp in n.index.levels[1]:
+                        try:
+                            info_text = "TRACE: " + str(day.date()) + "|TP " + \
+                                        str(int(tp)).zfill(2) + "|Mem=" + \
+                                        str(phymem_usage()[2]) + "%"
+                            logger.info(info_text)
+                            # get TP level data
+                            n2 = n.xs(day, level=0).xs(tp, level=0)\
+                                .reset_index('node', drop=True)
+                            b2 = b.xs(day, level=0).xs(tp, level=0)\
+                                .reset_index('branch', drop=True)
+                            # Perform downstream trace
+                            dfd, dfd1, dfd2, pl, pg = trans_use(b2, n2, nmap2,
+                                                                brmap, NPmap,
+                                                                downstream=True)
+                            dfds, dfds2 = sub_usage(dfd, pl, pg, nmap2, NPmap)
+                            td[str(tp)] = dfd1
+                            sd[str(tp)] = dfds
+                            # Perform upstream trace
+                            dfu, dfu1, dfu2, pl, pg = trans_use(b2, n2, nmap2,
+                                                                brmap, NPmap,
+                                                                downstream=False)
+                            dfus, dfus2 = sub_usage(dfu, pl, pg, nmap2, NPmap)
+                            tu[str(tp)] = dfu1
+                            su[str(tp)] = dfus
+                            fc[tp] = 0
+                        except Exception:
+                            logger.error("***FAILED*** for " + str(day) +
+                                         " trading period " + str(int(tp)) +
+                                         "***")
+                            logger.error(traceback.print_exc())
+                            fc[(day, tp)] = 1
+                            pass
+                    # monthly output filenames
+                    tuc = outpath + 'tu_' + ymd
+                    suc = outpath + 'su_' + ymd
+                    tdc = outpath + 'td_' + ymd
+                    sdc = outpath + 'sd_' + ymd
+                    # log
+                    logger.info(21*'=')
+                    logger.info("|OUTPUT: " + tuc + '|')
+                    logger.info("|OUTPUT: " + suc + '|')
+                    logger.info("|OUTPUT: " + tdc + '|')
+                    logger.info("|OUTPUT: " + sdc + '|')
+                    # spit
+                    # dailys to csv
+                    pd.Panel(tu).mean(0).to_csv(tuc)
+                    pd.Panel(su).mean(0).to_csv(suc)
+                    pd.Panel(td).mean(0).to_csv(tdc)
+                    pd.Panel(sd).mean(0).to_csv(sdc)
+
+fc = pd.Series(fc).to_csv(outpath + 'fc.csv')
